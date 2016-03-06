5e76b68cc06ea30c0b2d493e9a2e37c760e65f95
Alexey Yefimenko <alesha95@ukr.net>
2016-03-06 18:32:37 +0200

    домашка к 4 модулю

---
+++ module4/Area.java
@@ -0,0 +1,16 @@
+package module4;
+
+public class Area {
+
+    public static double treug(double a, double h){
+        return 0.5*a*h;
+    }
+
+    public static double pryamoug(double a, double b){
+        return a*b;
+    }
+
+    public static double krug(double r){
+        return 3.14*r*r;
+    }
+}
---
+++ module4/Farangeyt.java
@@ -0,0 +1,11 @@
+package module4;
+
+public class Farangeyt {
+    public static double farang(double c){
+        return (9/5)*c+32;
+    }
+
+    public static double celsiy(double f){
+        return (5/9)*(f-32);
+    }
+}
