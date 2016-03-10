ee1c6f2ed4e3340eba8f3b6780be33b46f9714c9
Alexey Yefimenko <alesha95@ukr.net>
2016-03-10 17:53:28 +0200

    Дз к 5 модулю

---
+++ module5/Array.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args){
+        int[] array = new int[5];
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите массив целых числе из 5 элементов:");
+
+        for (int i = 0; i <= 4; i++){
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Максимальное число: " + max(array));
+        System.out.println("Минимальное число: " + min(array));
+        System.out.println("Отсортированый массив: " + Arrays.toString(sorting(array)));
+    }
+
+    public static int max(int[] array){
+        int max = array[0];
+        for(int i = 0; i <= array.length - 1; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] array){
+        int min = array[0];
+        for(int i = 0; i <= array.length - 1; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int[] sorting(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+}
